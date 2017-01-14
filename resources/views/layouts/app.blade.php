5d18280d49ccb0cae37e80ad20fab08d2af37dfe
iberno <iberno@outlook.com>
2017-01-14 00:56:34 -0200

    Include Nav Links and Routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,19 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    ACL <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{!! route('permissions.index') !!}">Permissions</a>
+                                        <a href="{!! route('roles.index') !!}">Roles</a>
+                                        <a href="{!! route('users.index') !!}">Users</a>
+                                    </li>
+                                </ul>
+                            </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +7,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('permissions', 'ACL\PermissionController', ['except' => 'show']);
+Route::resource('roles', 'ACL\RoleController', ['except' => 'show']);
+Route::resource('users', 'ACL\UserController', ['except' => 'show']);
 Route::get('/home', 'HomeController@index');
